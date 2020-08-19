e35f83d427035f595676ecfb6949ef82ac635d18
Ann Impalovskaya <covertover@gmail.com>
2020-08-19 18:44:53 +0300

    Deck of cards

--- bin/cards_app.dart
+++ bin/cards_app.dart
@@ -1,10 +1,40 @@
-void main() {}
+void main() {
+  new Deck();
+}
 
 class Deck {
   List<Card> cards;
+
+  Deck() {
+    var ranks = [
+      'Ace',
+      'Two',
+      'Three',
+      'Four',
+      'Five',
+      'Six',
+      'Seven',
+      'Eight',
+      'Nine',
+      'Ten',
+      'Jack',
+      'Queen',
+      'King'
+    ];
+    var suits = ['Diamonds', 'Hearts', 'Spades', 'Clubs'];
+
+    for (var suit in suits) {
+      for (var rank in ranks) {
+        var card = new Card(suit, rank);
+        cards.add(card);
+      }
+    }
+  }
 }
 
 class Card {
   String suit;
   String rank;
+
+  Card(this.rank, this.suit);
 }
